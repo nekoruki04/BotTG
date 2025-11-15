14f33fe5b7eb5608c587f8164e33ef53faea77a3
nekoruki04 <levikietchinh1@gmail.com>
2025-11-15 17:03:12 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+
+API_TOKEN = "YOUR_BOT_TOKEN"
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Bạn vừa nhắn: {message.text}")
+
+print("Bot is running...")
+bot.infinity_polling()
